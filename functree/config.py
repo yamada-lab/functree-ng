4aef59077f71c238a3fb4d0460a3e62b26e0e384
Yuta Yamate <yyamate@bio.titech.ac.jp>
2017-09-10 02:57:19 +0900

    Add local time zone support

--- functree/config.py
+++ functree/config.py
@@ -4,6 +4,7 @@
 # or newly create "instance/config.py" with the same format.
 #
 DEBUG = False
+TIME_ZONE = 'UTC'
 SECRET_KEY = 'Replace me with long random characters!'
 MONGODB_HOST = 'mongodb://127.0.0.1:27017/functree?tz_aware=true'
 JSONIFY_PRETTYPRINT_REGULAR = False
