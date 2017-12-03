63634bd4eadbf103f1c41ea3fdf2fedc0b496c13
Yuta Yamate <yyamate@bio.titech.ac.jp>
2017-12-03 19:25:32 +0900

    Change name of configuration key

--- functree/config.py
+++ functree/config.py
@@ -4,10 +4,10 @@
 # or newly create "instance/config.py" with the same format.
 #
 DEBUG = False
-TIME_ZONE = 'UTC'
 SECRET_KEY = 'Replace me with long random characters!'
 MONGODB_HOST = 'mongodb://127.0.0.1:27017/functree?tz_aware=true'
 JSONIFY_PRETTYPRINT_REGULAR = False
+FUNCTREE_TIME_ZONE = 'UTC'
 FUNCTREE_ADMIN_USERNAME = 'admin'
 FUNCTREE_ADMIN_PASSWORD = 'password'
 FUNCTREE_PROFILE_TTL_DAYS = 7
