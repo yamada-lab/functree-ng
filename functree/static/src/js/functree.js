30b281bf88dc252842c12589a49aa43b7fddf90d
omixer <youssef.darzi@gmail.com>
2019-03-07 19:39:17 +0900

    Addition of a slider for ipath values cutoff and replacement of bower by yarn

--- functree/static/src/js/functree.js
+++ functree/static/src/js/functree.js
@@ -943,3 +943,26 @@ function zoomClick(zoom, config) {
     
     interpolateZoom([view.x, view.y], view.k, zoom);
 }
+
+/**
+ * Matcher for the typeahead plugin
+ * @param strs
+ * @returns
+ */
+var substringMatcher = function (strs) {
+	return function findMatches(q, cb) {
+		// an array that will be populated with substring matches
+		var matches = [];
+		// regex used to determine if a string contains the substring `q`
+		var substrRegex = new RegExp(q, 'i');
+
+		// iterate through the pool of strings and for any string that
+		// contains the substring `q`, add it to the `matches` array
+		$.each(strs, function(i, str) {
+			if (substrRegex.test(str)) {
+				matches.push(str);
+			}
+		});
+		cb(matches);
+	};
+};
